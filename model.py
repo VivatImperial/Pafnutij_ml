d11a0d80edea78845cb857723a117e369fe28a77
Imperial Vivat <vsevolod.bogodist@gmail.com>
2023-07-20 23:28:32 +0300

    что-то куда-то подвинули

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 from tensorflow.keras import layers, models
 from tensorflow.keras.preprocessing.image import load_img, img_to_array, ImageDataGenerator
 
-def load_model_and_predict(pic, path='Pafnutij_ml\model'):
+def load_model_and_predict(pic, path='model'):
     my_image = load_img(pic, target_size=(224, 224))
 
     pafnutij = models.load_model(path)
